c7b3abf69ece56e62a3286420f04e86291b209ef
jpic <jamespic@gmail.com>
2012-11-30 10:40:06 +0100

    Added django-rules-light

--- hp/urls.py
+++ hp/urls.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
+import rules_light
+rules_light.autodiscover()
+
 import autocomplete_light
 autocomplete_light.autodiscover()
 
