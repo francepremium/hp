ef17ea600a5c6131205f495601c920098f8caaba
jpic <jamespic@gmail.com>
2012-12-18 16:57:21 +0100

    Support links in record_detail

--- formapp/templatetags/formapp_tags.py
+++ formapp/templatetags/formapp_tags.py
@@ -1,7 +1,19 @@
 from django import template
+from django.utils.safestring import mark_safe
+
+from formapp.models import Record
 
 register = template.Library()
 
+
+@register.filter
+def render_value(value):
+    if isinstance(value, list):
+        value = Record.objects.filter(pk__in=value)
+        value = mark_safe(u', '.join(
+            ['<a href="%s">%s</a>' % (
+                r.get_absolute_url(), r) for r in value]))
+    return value
 
 @register.filter
 def get_field(form, name):
