0460d95a4bcd9de018cafd94c3147d26c30abdba
jpic <jamespic@gmail.com>
2013-01-30 10:10:36 +0100

    Auto select 5 first columns on fusion.List creation

--- fusion/models.py
+++ fusion/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models import signals
 from django.core.urlresolvers import reverse
 
+from form_designer.models import Form, Widget
 from appstore.signals import post_app_install
 
 
@@ -12,3 +13,30 @@ class List(models.Model):
 
     def get_absolute_url(self):
         return reverse('fusion_list_detail', args=(self.feature.pk,))
+
+
+def list_created_initial_columns(sender, instance, created, **kwargs):
+    if instance.columns.count() > 0:
+        return
+
+    forms = Form.objects.filter(
+        appform__app__environment=instance.environment,
+        appform__app__provides=instance.feature,
+    ).order_by('-pk')
+
+    deployed_forms = forms.filter(appform__app__deployed=True)
+
+    if not deployed_forms.count():
+        if not forms.count():
+            return
+
+        form = forms[0]
+    else:
+        form = deployed_forms[0]
+
+    widgets = Widget.objects.filter(tab__form=form).order_by(
+        'tab', 'tab__order', 'order')[:5]
+
+    for widget in widgets:
+        instance.columns.add(widget)
+signals.post_save.connect(list_created_initial_columns, sender=List)
