ac96f8237f1d2fe4cb452d646b2585481c3f47ea
jpic <jamespic@gmail.com>
2013-02-01 12:48:35 +0100

    Cleaned dragtable script

--- hp/static/fusion/jquery.dragtable.js
+++ hp/static/fusion/jquery.dragtable.js
@@ -1,101 +1,154 @@
 if (window.yourlabs == undefined) window.yourlabs = {};
 
 yourlabs.Table = function(table) {
+    // <table> element.
     this.table = table;
-    this.draggingIndex = null;
-    this.droppableIndex = null;
+
+    // Index of the column being drag.
+    this.dragIndex = null;
+
+    // Index of the column being hovered or drop on.
+    this.dropIndex = null;
+
+    // Drag column handle which will be prepended to th.
+    this.dragHandleHtml = '<span class="handle drag-handle"></span>';
+
+    // Selector string for dragHandleHtml.
+    this.dragHandleSelector = '.drag-handle';
+
+    // CSS class for the column being drag. Will be set on all td, ths.
+    this.dragColumnClass = 'dragging';
+
+    // Selector string for the column being drag.
+    this.dragColumnSelector = '.' + this.dragColumnClass;
+
+    // HTML used to create the semi transparent drag helper. It will be filled
+    // with copies of the contents of the column being drag.
+    this.helperTableHtml = '<table class="helper">';
+
+    // Cell HTML used to create placeholder columns.
+    this.placeholderHtml = '<td class="placeholder"></td>';
+
+    // Selector string for placeholderHtml
+    this.placeholderSelector = '.placeholder';
+
+    // CSS class that should be added to placeholder when they become active.
+    this.placeholderActiveClass = 'active';
+
+    // Selector string for active placeholders
+    this.placeholderActiveSelector = [
+        this.placeholderSelector,
+        '.',
+        this.placeholderActiveClass
+    ].join('')
 }
 
 yourlabs.Table.prototype.initialize = function() {
     this.table.find('th')
-        .prepend('<span class="handle"></span>')
+        .prepend(this.dragHandleHtml)
         .draggable({
             cursor: 'move',
-            helper: $.proxy(this.draggableHelper, this),
-            start: $.proxy(this.draggableStart, this),
+            helper: $.proxy(this.dragHelper, this),
+            start: $.proxy(this.dragStart, this),
             stop: $.proxy(this.stopDrag, this),
-            handle: '.handle',
+            handle: this.dragHandleSelector,
             tolerance: 'pointer',
             opacity: 0.65,
             axis: 'x',
         });
 
-    this.resetDroppable();
-}
-
-yourlabs.Table.prototype.resetDroppable = function() {
-    this.table.find('.placeholder').remove()
-
-    var placeholder = '<td class="placeholder"></td>';
-
-    this.table.find('th, td').before(placeholder);
-    this.table.find('tr').append(placeholder)
-
-    this.table.find('th, td:not(.placeholder)')
+    
+    this.table.find('th, td')
         .droppable({
-            hoverClass: 'active',
             greedy: true,
             tolerance: 'pointer',
-            over: $.proxy(this.droppableOver, this),
+            over: $.proxy(this.dropOver, this),
             drop: $.proxy(this.stopDrag, this),
         });
+    
+    this.createPlaceholders();
 }
 
-yourlabs.Table.prototype.draggableHelper = function(e) {
-    this.draggingIndex = $(e.currentTarget).index() + 1;
+// Remove all placeholder cells.
+yourlabs.Table.prototype.removePlaceholders = function() {
+    this.table.find(this.placeholderSelector).remove()
+}
+
+// Create a placeholder column after each actual column.
+yourlabs.Table.prototype.createPlaceholders = function() {
+    this.table.find('th, td').before(this.placeholderHtml);
+    this.table.find('tr').append(this.placeholderHtml)
+}
+
+yourlabs.Table.prototype.dragHelper = function(e) {
+    this.dragIndex = $(e.currentTarget).index() + 1;
 
     var rows = [];
 
-    this.getColumn(this.draggingIndex).each(function() {
+    this.getColumn(this.dragIndex).each(function() {
         rows.push('<tr>')
         rows.push($('<tr>').html($(this).clone()).html());
         rows.push('</tr>')
     })
 
-    return $('<table class="helper">').html(rows.join(''));
+    return $(this.helperTableHtml).html(rows.join(''));
 }
 
-yourlabs.Table.prototype.draggableStart = function(e, ui) {
-    this.draggingIndex = $(e.currentTarget).index() + 1;
-    this.getColumn(this.draggingIndex).addClass('dragging');
+// On drag start, note the dragged column index and set dragColumnClass.
+yourlabs.Table.prototype.dragStart = function(e, ui) {
+    this.dragIndex = $(e.currentTarget).index() + 1;
+    this.getColumn(this.dragIndex).addClass(this.dragColumnClass);
 }
 
+// On drag stop, get the drop column index, move the cells, trigger columnMoved
+// and clean up.
 yourlabs.Table.prototype.stopDrag = function(e, ui) {
-    if (this.draggingIndex == null) {
+    if (this.dragIndex == null) {
         // stopDrag was already called.
         return;
     }
 
-    this.droppableIndex = this.table.find('.placeholder.active:first').index();
-    this.table.find('.placeholder.active').removeClass('active')
-    this.table.find('.dragging').removeClass('dragging');
+    this.dropIndex = this.table.find(
+        this.placeholderActiveSelector + ':first').index();
 
-    var draggedColumn = this.getColumn(this.draggingIndex);
-    var droppedColumn = this.getColumn(this.droppableIndex);
+    this.table.find(this.placeholderActiveSelector)
+        .removeClass(this.placeholderActiveClass)
 
-    for (var i=0; i<draggedColumn.length; i++) {
-        $(draggedColumn.get(i)).insertAfter(droppedColumn.get(i))
+    this.table.find(this.dragColumnSelector)
+        .removeClass(this.dragColumnClass);
+
+    var dragColumn = this.getColumn(this.dragIndex);
+    var dropColumn = this.getColumn(this.dropIndex);
+
+    for (var i=0; i<dragColumn.length; i++) {
+        $(dragColumn.get(i)).insertAfter(dropColumn.get(i))
     }
 
-    this.draggingIndex = null;
-    this.droppableIndex = null;
+    this.dragIndex = null;
+    this.dropIndex = null;
 
+    this.removePlaceholders();
+    // Trigger columnMoved while the table is clean from fake columns.
     this.table.trigger('columnMoved');
-
-    this.resetDroppable();
+    this.createPlaceholders();
 }
 
+// Return all cells of a particular column.
 yourlabs.Table.prototype.getColumn = function(index) {
-    return this.table.find('td:nth-child(' + index + '), th:nth-child(' + index + ')');
+    return this.table.find(
+        'td:nth-child(' + index + '), th:nth-child(' + index + ')');
 }
 
-yourlabs.Table.prototype.droppableOver = function(e, ui) {
-    this.droppableIndex = $(e.target).index() + 1;
+// On drop over, update active placeholder.
+yourlabs.Table.prototype.dropOver = function(e, ui) {
+    this.dropIndex = $(e.target).index() + 1;
 
-    this.activePlaceholder = this.droppableIndex + 1;
+    this.activePlaceholder = this.dropIndex + 1;
 
-    this.table.find('.placeholder').removeClass('active')
-    this.getColumn(this.activePlaceholder).addClass('active')
+    this.table.find(this.placeholderSelector)
+        .removeClass(this.placeholderActiveClass)
+    this.getColumn(this.activePlaceholder)
+        .addClass(this.placeholderActiveClass)
 }
 
 $.fn.yourlabsTable = function(overrides) {
