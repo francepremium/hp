e1e0b133275ee081a09b8047bfe11593c0e016b3
jpic <jamespic@gmail.com>
2013-02-20 12:15:54 +0100

    Require login in formapp

--- formapp/urls.py
+++ formapp/urls.py
@@ -1,25 +1,26 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 import views
 
 
 urlpatterns = patterns('',
     url(r'search/$',
-        views.Search.as_view(),
+        login_required(views.Search.as_view()),
         name='formapp_record_search'),
     url(r'(?P<feature_pk>\d+)/list/$',
-        views.List.as_view(),
+        login_required(views.List.as_view()),
         name='formapp_record_list'),
     url(r'(?P<app_pk>\d+)/create/$',
-        views.Create.as_view(),
+        login_required(views.Create.as_view()),
         name='formapp_record_create'),
     url(r'(?P<pk>\d+)/update/$',
-        views.Update.as_view(),
+        login_required(views.Update.as_view()),
         name='formapp_record_update'),
     url(r'(?P<pk>\d+)/detail/$',
-        views.Detail.as_view(),
+        login_required(views.Detail.as_view()),
         name='formapp_record_detail'),
     url(r'(?P<pk>\d+)/delete/$',
-        views.Delete.as_view(),
+        login_required(views.Delete.as_view()),
         name='formapp_record_delete'),
 )
